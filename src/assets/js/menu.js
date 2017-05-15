be5b90ba86db7ee5fe165ecedbc8c9a69d0c2fe2
SoAlenax <soalenaxlyx@yahoo.com>
2017-05-15 11:00:58 +0300

    part 1

---
+++ src/assets/js/menu.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+(() => {
+    'use strict';
+
+
+
+})();
+
+/*
+    window.addEventListener("resize", function() {
+    var mapElement = document.getElementById("ax__main");
+    mapElement.style.height = mapElement.offsetWidth / 2;
+});
+*/
